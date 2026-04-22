ce21413d3bc68b1a90ebbac60da880965dc7937c
Desrielkiki <desriel7c6@gmail.com>
2026-04-22 17:46:49 +0700

    finalize task capstone5-filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,12 +2,13 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
-$routes->get('/produk', 'ProdukController::index');
-$routes->get('/keranjang', 'KeranjangController::index');
+/** @var RouteCollection $routes */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
+
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'KeranjangController::index', ['filter' => 'auth']);
